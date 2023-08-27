1bd63558b4f7a4eccda68dc04d0a3935dfb5ad89
Tiny Taimi <OpNop@users.noreply.github.com>
2023-08-26 20:36:34 -0700

    Added Secrets of the Obscure's Sky Lakes

---
+++ SkyLake.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace Taimi.UndaDaSea_BlishHUD
+{
+    // Thank to Teh from the BlishHUD discord for this class 🥰
+    public class SkyLake
+    {
+        private readonly float _waterSurface;
+        private readonly float _waterBottom;
+        private readonly List<Vector3> bounds;
+        private readonly Vector3 center;
+        private readonly float radius;
+        private float _distance;
+
+        public SkyLake(float waterSurface, float waterBottom, List<Vector3> bounds)
+        {
+            _waterSurface = waterSurface;
+            _waterBottom = waterBottom;
+            this.bounds = bounds;
+            center = GetCenter();
+            radius = GetRadius();
+        }
+
+        public float WaterSurface
+        {
+            get { return _waterSurface; }
+        }
+
+        public float Distance
+        {
+            get { return _distance; }
+        }
+
+        //so we don't have to perform the more expensive calcs all the time
+        public bool IsNearby(Vector3 playerPos)
+        {
+            _distance = Vector3.Distance(playerPos, center);
+            if (_distance > radius * 1.5) return false; // arbitrary distance
+
+            return true;
+        }
+
+        //water check
+        public bool IsInWater(Vector3 playerPos)
+        {
+            //if not within top/bottom, not in water
+            if (playerPos.Z < _waterBottom || playerPos.Z > (_waterSurface + 10))
+                return false;
+
+            return IsInPolygon(playerPos);
+        }
+
+        //Point in Polygon algorithm
+        private bool IsInPolygon(Vector3 playerPos)
+        {
+            var count = bounds.Count;
+            bool isInside = false;
+
+            for (int i = 0, j = count - 1; i < count; j = i++)
+            {
+                bool isYAboveFirstVertex = bounds[i].Y > playerPos.Y;
+                bool isYAboveSecondVertex = bounds[j].Y > playerPos.Y;
+
+                if (isYAboveFirstVertex != isYAboveSecondVertex)
+                {
+                    float intersectionX = bounds[i].X + (playerPos.Y - bounds[i].Y) / (bounds[j].Y - bounds[i].Y) * (bounds[j].X - bounds[i].X);
+                    if (playerPos.X < intersectionX)
+                    {
+                        isInside = !isInside;
+                    }
+                }
+            }
+
+            return isInside;
+        }
+
+        //get center of polygon
+        private Vector3 GetCenter()
+        {
+            Vector3 center = new Vector3(0, 0, 0);
+
+            foreach (var bound in bounds)
+            {
+                center += bound;
+            }
+
+            center /= bounds.Count;
+            return center;
+        }
+
+        //get radius of polygon
+        private float GetRadius()
+        {
+            float radius = 0;
+            foreach (var bound in bounds)
+            {
+                float distance = Vector3.Distance(bound, center);
+                if (distance > radius)
+                    radius = distance;
+            }
+
+            return radius;
+        }
+    }
+}
--- UndaDaSea_BlishHUD.cs
+++ UndaDaSea_BlishHUD.cs
@@ -1,9 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
-using System.Reflection;
+using System.Linq;
 using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Windows.Forms.VisualStyles;
 using Blish_HUD;
 using Blish_HUD.Modules;
 using Blish_HUD.Modules.Managers;
@@ -48,10 +47,13 @@ namespace Taimi.UndaDaSea_BlishHUD
 
         protected override async Task LoadAsync()
         {
-            
+
         }
 
         VolumeSampleProvider _volumeSampler;
+        List<SkyLake> _skyLakes;
+        int[] SkyLakeMaps = {1510, 1517, 1509};
+        float waterOffset = 0;
 
         protected override void OnModuleLoaded(EventArgs e)
         {
@@ -69,6 +71,9 @@ namespace Taimi.UndaDaSea_BlishHUD
             //Catch when the games is closed and started to bring on the music
             GameService.GameIntegration.Gw2Instance.Gw2Closed  += GameIntegration_Gw2Closed;
             GameService.GameIntegration.Gw2Instance.Gw2Started += GameIntegration_Gw2Started;
+
+            //Load SkyLakes
+            LoadSkyLakes();
 
             // Base handler must be called
             base.OnModuleLoaded(e);
@@ -98,16 +103,30 @@ namespace Taimi.UndaDaSea_BlishHUD
             _timeSinceUpdate = 0;
 
             //REMEMBER: Blish crossed his wires, Z=Y and Y=Z
-            //For BlishOS use Zloc
-            float Zloc = GameService.Gw2Mumble.PlayerCharacter.Position.Z;
-            float volume;
+            //For BlishOS use Z
+            var playerPosition = GameService.Gw2Mumble.PlayerCharacter.Position;
+            var CurrentMap = GameService.Gw2Mumble.CurrentMap.Id;
+            var volume = 0f;
 
-            if (Zloc <= 0) {
+            //SotO "Sky lakes"
+            if (SkyLakeMaps.Contains(CurrentMap))
+            {
+                //Check if they are near/in a Sky Lake
+                var NearbyLake = _skyLakes.Where(lake => lake.IsNearby(playerPosition)).OrderBy(lake => lake.Distance).FirstOrDefault();
+                waterOffset = (NearbyLake != null && NearbyLake.IsInWater(playerPosition)) ? NearbyLake.WaterSurface : 0;
+            } 
+            else
+            {
+                //Default water level
+                waterOffset = 0;
+            }
+
+            if (playerPosition.Z <= waterOffset) {
                 //Dey unda the sea, LET THE BLOWFISH BLOW 
-                volume = Map(Zloc, -30, 0, (_masterVolume.Value / 100), 0.01f);
+                volume = Map(playerPosition.Z, waterOffset - 30, waterOffset, (_masterVolume.Value / 100), 0.01f);
             } else {
                 //Getting "near" the sea, give 'em a sample of undersea life
-                volume = Map(Zloc, 0, 3, 0.01f, 0f);
+                volume = Map(playerPosition.Z, waterOffset, waterOffset + 3, 0.01f, 0f);
             }
 
             //Lets not get crazy here, keep it between 0 and 1
@@ -153,8 +172,84 @@ namespace Taimi.UndaDaSea_BlishHUD
         protected override void Unload()
         {
             // Unload
-            _soundClip.Stop();
-            _soundClip.Dispose();
+            _soundClip?.Stop();
+            _soundClip?.Dispose();
+            _skyLakes?.Clear();
+        }
+
+        //Lame Sky Lake Data Loader
+        private void LoadSkyLakes()
+        {
+            _skyLakes = new List<SkyLake>
+            {
+                //Jade Mech Habitation Zone 03
+                new SkyLake(396, 300, new List<Vector3> {
+                    new Vector3(77,   -53, 402),
+                    new Vector3(77,   115, 402),
+                    new Vector3(-155, 115, 402),
+                    new Vector3(-155, -53, 402),
+                }),
+
+                //Stargaze Ridge
+                new SkyLake(260, 200, new List<Vector3> {
+                    new Vector3(1038, 263, 260),
+                    new Vector3(1111, 252, 260),
+                    new Vector3(1100, 35,  260),
+                    new Vector3(1012, 85,  260),
+                }),
+
+                //Primal Maguuma (Low lake)
+                new SkyLake(353, 300, new List<Vector3>
+                {
+                    new Vector3(-83, 584, 353),
+                    new Vector3(-81, 626, 353),
+                    new Vector3(-24, 634, 353),
+                    new Vector3(52, 594, 353),
+                    new Vector3(85, 485, 353),
+                    new Vector3(195, 467, 353),
+                    new Vector3(208, 423, 353),
+                    new Vector3(157, 386, 353),
+                    new Vector3(152, 317, 353),
+                    new Vector3(56, 310, 353),
+                    new Vector3(18, 376, 353),
+                    new Vector3(22, 416, 353),
+                    new Vector3(-20, 438, 353),
+                    new Vector3(-29, 538, 353),
+                }),
+
+                //Primal Maguuma (mid lake)
+                new SkyLake(435, 400, new List<Vector3> {
+                    new Vector3(8.762414f, 365.2565f, 435),
+                    new Vector3(2.563565f, 408.4081f, 435),
+                    new Vector3(-22.61929f, 438.4842f, 435),
+                    new Vector3(-45.60424f, 500.1057f, 435),
+                    new Vector3(-73.58975f, 559.9127f, 435),
+                    new Vector3(-126.0663f, 559.3304f, 435),
+                    new Vector3(-125.1464f, 467.7312f, 435),
+                    new Vector3(-80.47857f, 398.7868f, 435),
+                    new Vector3(-54.03018f, 361.1153f, 435),
+                    new Vector3(-6.560186f, 349.9526f, 435),
+                }),
+
+                //Primal Maguuma (high lake)
+                new SkyLake(484, 430, new List<Vector3> {
+                    new Vector3(-121.9899f, 437.0892f, 484),
+                    new Vector3(-157.1637f, 462.3725f, 484),
+                    new Vector3(-167.7869f, 471.4856f, 484),
+                    new Vector3(-170.0892f, 497.2692f, 484),
+                    new Vector3(-243.7502f, 511.4792f, 484),
+                    new Vector3(-243.0668f, 466.6464f, 484),
+                    new Vector3(-221.771f, 439.689f, 484),
+                }),
+
+                //Primal Maguuma (top pond)
+                new SkyLake(494, 483, new List<Vector3> {
+                    new Vector3(-319.1087f, 516.947f, 494),
+                    new Vector3(-248.7634f, 511.2591f, 494),
+                    new Vector3(-250.345f, 480.397f, 494),
+                    new Vector3(-313.2242f, 487.2502f, 494),
+                }),
+            };
         }
 
     }
